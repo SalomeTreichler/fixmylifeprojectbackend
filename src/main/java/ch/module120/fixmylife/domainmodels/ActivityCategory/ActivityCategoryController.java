9f7a96b1151a856c50e1c3dbf326e800987ada47
Salome Treichler <salome.treichler@noseryoung.ch>
2020-06-23 16:38:51 +0200

    Activity Category All done

--- src/main/java/ch/module120/fixmylife/domainmodels/ActivityCategory/ActivityCategoryController.java
+++ src/main/java/ch/module120/fixmylife/domainmodels/ActivityCategory/ActivityCategoryController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/activityCategories")
 public class ActivityCategoryController {
     private ActivityCategoryService activityCategoryService;
 
@@ -17,35 +19,35 @@ public class ActivityCategoryController {
         this.activityCategoryService = activityCategoryService;
     }
 
-    // localhost:8080/activities  GET
+    // localhost:8080/activityCategories  GET
     @GetMapping("")
     public @ResponseBody
     ResponseEntity<List<ActivityCategory>> getAll() {
         return new ResponseEntity<>(activityCategoryService.getAll(), HttpStatus.OK);
     }
 
-    // localhost:8080/activities/{id} GET
+    // localhost:8080/activityCategories/{id} GET
     @GetMapping("/{id}")
     public @ResponseBody
     ResponseEntity<ActivityCategory> getById(@PathVariable String id) {
         return new ResponseEntity<>(activityCategoryService.getById(id), HttpStatus.OK);
     }
 
-    // localhost:8080/activities POST
+    // localhost:8080/activityCategories POST
     @PostMapping("")
     public @ResponseBody
     ResponseEntity<ActivityCategory> create(@RequestBody ActivityCategory activityCategory) {
         return new ResponseEntity<>(activityCategoryService.create(activityCategory), HttpStatus.CREATED);
     }
 
-    //localhost:8080/activities UPDATE
+    //localhost:8080/activityCategories UPDATE
     @PutMapping("/{id}")
     public @ResponseBody
     ResponseEntity<ActivityCategory> update(@PathVariable String id, @RequestBody ActivityCategory activityCategory){
         return new ResponseEntity<>(activityCategoryService.update(id, activityCategory), HttpStatus.OK);
     }
 
-    // localhost:8080/activities/{id} DELETE
+    // localhost:8080/activityCategories/{id} DELETE
     @DeleteMapping("/{id}")
     public @ResponseBody
     ResponseEntity<String> delete(@PathVariable String id){
