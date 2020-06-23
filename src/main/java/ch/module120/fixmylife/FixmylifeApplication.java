fe1b82d1870ae2ce438b131a440f3851edea25bf
Salome Treichler <salome.treichler@noseryoung.ch>
2020-06-23 15:35:55 +0200

    initialized project

---
+++ src/main/java/ch/module120/fixmylife/FixmylifeApplication.java
@@ -0,0 +1,13 @@
+package ch.module120.fixmylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixmylifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixmylifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/module120/fixmylife/FixmylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.module120.fixmylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixmylifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
